baeb8eb0a3072841113f9e32830f2a486765b7c0
MSmaga <msmaga@protonmail.com>
2022-02-06 10:39:40 +0200

    Deleted wrong Libs

--- ClassLibrary1/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ClassLibrary1
-{
-    public class Class1
-    {
-        public void PublicMethod() => Console.WriteLine("This is the TestMethod is working...");
-    }
-}
